5889bdaa2f35719a18af82797c3dd9440e738ef3
Ahmed Saeed <115148623+ahmed-s-fatahallah@users.noreply.github.com>
2024-05-03 17:18:21 +0300

    feat: Get more product data and update the realtime database with them

--- firebaseInit.ts
+++ firebaseInit.ts
@@ -6,6 +6,7 @@ import {
   getApps,
 } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
+import { getDatabase } from "firebase-admin/database";
 
 import myCred from "./react-http-47f95-firebase-adminsdk-ryepv-56ed444eb9.json" assert { type: "json" };
 
@@ -15,7 +16,9 @@ export const app =
     : initializeApp({
         credential: cert(myCred as ServiceAccount),
         storageBucket: "react-http-47f95.appspot.com",
+        databaseURL: "https://react-http-47f95-default-rtdb.firebaseio.com",
       });
 
 export const storage = getStorage();
 export const bucket = storage.bucket();
+export const database = getDatabase();
--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import puppeteer from "puppeteer";
 import fs from "fs-extra";
 import path, { dirname } from "path";
 import { fetchWithRetry } from "./utils.js";
-import { bucket } from "./firebaseInit.js";
+import { bucket, database } from "./firebaseInit.js";
 import { getDownloadURL } from "firebase-admin/storage";
 import { fileURLToPath } from "url";
 
@@ -21,10 +21,57 @@ const imagesFolderPath = path.join(
   "../images"
 );
 
-const downloadImages = async (
-  imgsSrcArr: (string | undefined)[],
-  title: string | null | undefined
-) => {
+const getProductDataWithPuppeteer = async () => {
+  const browser = await puppeteer.launch(config);
+  const page = await browser.newPage();
+  await page.goto(URL);
+  await page.waitForNetworkIdle();
+  const productObj = await page.evaluate(() => {
+    const imgsSrcArr = Array.from<HTMLImageElement>(
+      document.querySelectorAll(".Carousel img")
+    ).map((img) => {
+      const src = img.getAttribute("data-src");
+      if (src) return src;
+
+      return "";
+    });
+    const imgsSrcFiltered = Array.from(new Set(imgsSrcArr));
+
+    // to arrange the urls correctly
+    imgsSrcFiltered.push(imgsSrcFiltered.shift() ?? "");
+
+    const productRoute = window.location.pathname.split("/")[2];
+
+    const title = document.querySelector("h1")?.textContent;
+
+    const colorName =
+      document.querySelector(".Overview__name")?.textContent ?? "";
+
+    const colorBtnEl = document.querySelector(".ColorSwatch");
+
+    let rgbValue = "";
+    if (colorBtnEl) rgbValue = getComputedStyle(colorBtnEl)?.backgroundImage;
+    const colorType =
+      document
+        .querySelector(".Overview__name")
+        ?.previousSibling?.textContent?.substring(0, 7)
+        ?.toLowerCase() ?? "";
+
+    return {
+      imgsSrcFiltered,
+      title,
+      productRoute,
+      colorName,
+      rgbValue,
+      colorType,
+    };
+  }, []);
+
+  await browser.close();
+  return productObj;
+};
+
+const downloadImages = async (imgsSrcArr: string[], title: string) => {
   for (let i = 0; i < imgsSrcArr.length; i++) {
     const productImg = imgsSrcArr[i];
     if (productImg?.includes(".jpg")) continue;
@@ -53,55 +100,78 @@ const downloadImages = async (
   }
 };
 
-const uploadImagesToFireBaseStorage = async (
-  title: string | null | undefined
-) => {
+const uploadImagesToFireBaseStorage = async (title: string) => {
   if (!fs.existsSync(imagesFolderPath)) return;
 
   const imagesFilesArr = fs.readdirSync(imagesFolderPath);
 
-  for (let imageFile of imagesFilesArr) {
-    const file = bucket.file(`productsImages/${title}/${imageFile}`);
+  const uploadPromises = imagesFilesArr.map((imageFile) => {
+    return new Promise((resolve: (downloadUrl: string) => void, reject) => {
+      const file = bucket.file(`productsImages/${title}/${imageFile}`);
 
-    const fileWriteStream = file.createWriteStream();
+      const fileWriteStream = file.createWriteStream();
 
-    fileWriteStream.on("error", (error) => {
-      console.error(
-        "Something is wrong! Unable to upload at the moment." + error
+      fileWriteStream.on("error", (error) => {
+        console.error(
+          "Something is wrong! Unable to upload at the moment." + error
+        );
+        reject(error);
+      });
+
+      fileWriteStream.on("finish", async () => {
+        const downloadUrl = await getDownloadURL(file);
+        console.log(`Image uploaded ${imageFile}`);
+        resolve(downloadUrl);
+      });
+
+      fileWriteStream.end(
+        fs.readFileSync(`${imagesFolderPath}/${imageFile}`),
+        () => {
+          fs.removeSync(`${imagesFolderPath}/${imageFile}`);
+        }
       );
     });
+  });
 
-    fileWriteStream.on("finish", async () => {
-      const downloadUrl = await getDownloadURL(file);
-      console.log(downloadUrl);
-    });
+  return await Promise.all(uploadPromises);
+};
 
-    fileWriteStream.end(fs.readFileSync(`${imagesFolderPath}/${imageFile}`));
-  }
+const updateDataBase = async (
+  imagesUrls: string[],
+  title: string,
+  colorName: string,
+  rgb: string,
+  type: string,
+  productRoute: string
+) => {
+  const productObjRef = database.ref("products");
+  await productObjRef.child(`${productRoute}/colors/0/imgs`).set(imagesUrls);
+  await productObjRef
+    .child(`${productRoute}/colors/0`)
+    .update({ colorName, rgb, type, sliderImg: imagesUrls[0] });
+  await productObjRef.child(productRoute).update({ title });
+  database.goOffline();
+
+  console.log("Database Updated");
 };
 
 (async () => {
-  const browser = await puppeteer.launch(config);
-  const page = await browser.newPage();
-
-  await page.goto(URL);
-
-  await page.waitForNetworkIdle();
-
-  const productObj = await page.evaluate(() => {
-    const imgsSrcArr = Array.from<HTMLImageElement>(
-      document.querySelectorAll(".Carousel img")
-    ).map((img) => {
-      const src = img.getAttribute("data-src");
-      if (src) return src;
-    });
-    const imgsSrcFiltered = Array.from(new Set(imgsSrcArr));
-
-    const title = document.querySelector("h1")?.textContent;
-    return { imgsSrcFiltered, title };
-  }, []);
+  const productObj = await getProductDataWithPuppeteer();
+  if (!productObj.imgsSrcFiltered || !productObj.title) return;
 
   await downloadImages(productObj.imgsSrcFiltered, productObj.title);
-  await uploadImagesToFireBaseStorage(productObj.title);
-  await browser.close();
+
+  const imagesUrlsArr = await uploadImagesToFireBaseStorage(productObj.title);
+
+  if (!imagesUrlsArr)
+    throw new Error("Uploading images to firebase storage failed");
+
+  await updateDataBase(
+    imagesUrlsArr,
+    productObj.title,
+    productObj.colorName,
+    productObj.rgbValue,
+    productObj.colorType,
+    productObj.productRoute
+  );
 })();
